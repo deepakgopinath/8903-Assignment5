3aab9676d61757027af69b468df31e5dedd22b61
Deepak Gopinath <dedakkattilgopinath@berklee.edu>
2014-04-06 16:19:11 -0400

    "Added CFeatureExtractor"

---
+++ ProjectTemplate-master/MyProject/incl/CFeatureExtractor.h
@@ -0,0 +1,14 @@
+//
+//  CFeatureExtractor.h
+//  MyProject
+//
+//  Created by Deepak on 4/6/14.
+//
+//
+
+#ifndef __MyProject__CFeatureExtractor__
+#define __MyProject__CFeatureExtractor__
+
+#include <iostream>
+
+#endif /* defined(__MyProject__CFeatureExtractor__) */
---
+++ ProjectTemplate-master/MyProject/src/MyProject/CFeatureExtractor.cpp
@@ -0,0 +1,9 @@
+//
+//  CFeatureExtractor.cpp
+//  MyProject
+//
+//  Created by Deepak on 4/6/14.
+//
+//
+
+#include "CFeatureExtractor.h"
